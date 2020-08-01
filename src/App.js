cda9ae86550c1f3658b47b7f82c71bf1212d4cd0
danish45007 <myemail>
2020-08-02 02:46:41 +0530

    final commit --hopefully �

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import Job from './Job';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import Loader from 'react-loader-spinner';
 import JobsPagination from "./JobsPagination"
+import SerachForm from './SerachForm';
 
 
 const App  = () => {
   const [params,setParams] = useState({})
   const [page,setPage] = useState(1)
   const { jobs,loading,error,hasNextPage } = useFetchJobs(params,page)
+   
+  function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+  }
+
   return (
     <Container className="my-5">
       {loading && <Loader 
@@ -24,6 +35,7 @@ const App  = () => {
      />}
       {error && <h1>Error....</h1>}
       <h1 style={{fontFamily:'Anton'}} className="d-flex align-items-center justify-content-center">GitHub Jobs</h1>
+      <SerachForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} hasNextPage={hasNextPage} setPage={setPage} />
       {jobs.map(job => {
         return <Job key={job.id} job={job}></Job>
