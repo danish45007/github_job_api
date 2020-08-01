5a928369fce23cce96f93a1d199d9e81e3243455
danish45007 <danish45007@gmail.com>
2020-08-02 03:01:35 +0530

    Updates

---
+++ precache-manifest.b1e35e7e34b1ed4bda8d3601f0705b32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a977ba6f4037f6680393c574c11abc",
+    "url": "/github_job_api/index.html"
+  },
+  {
+    "revision": "d9f7d229bed0806e2f4f",
+    "url": "/github_job_api/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "ebb1990900d1493aa3cd",
+    "url": "/github_job_api/static/css/main.176b7693.chunk.css"
+  },
+  {
+    "revision": "d9f7d229bed0806e2f4f",
+    "url": "/github_job_api/static/js/2.84200ba1.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/github_job_api/static/js/2.84200ba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb1990900d1493aa3cd",
+    "url": "/github_job_api/static/js/main.4223cce5.chunk.js"
+  },
+  {
+    "revision": "d4b8762205279d23512f",
+    "url": "/github_job_api/static/js/runtime-main.fd7b160d.js"
+  }
+]);
